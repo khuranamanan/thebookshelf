aebdfcc229b735d201a350e7a0cfdacbf3c97692
Manan Khurana <112554344+khuranamanan@users.noreply.github.com>
2023-05-18 17:06:44 +0530

    feat: signUp Feature Complete

--- src/Contexts/AuthContext.jsx
+++ src/Contexts/AuthContext.jsx
@@ -1,5 +1,6 @@
 import { createContext, useState } from "react";
 import logInService from "../Services/logInService";
+import signUpService from "../Services/signUpService";
 
 export const AuthContext = createContext();
 
@@ -37,8 +38,37 @@ function AuthProvider({ children }) {
     }
   }
 
+  async function signUpUser(email, password, firstName, lastName) {
+    console.log("sending this:", email, password, firstName, lastName);
+    try {
+      const response = await signUpService(
+        email,
+        password,
+        firstName,
+        lastName
+      );
+      const { createdUser, encodedToken } = response.data;
+
+      setLoginData({
+        token: encodedToken,
+        user: createdUser,
+        isLoggedIn: true,
+        isError: null,
+      });
+
+      localStorage.setItem("token", JSON.stringify({ token: encodedToken }));
+      localStorage.setItem("user", JSON.stringify({ user: createdUser }));
+    } catch (err) {
+      setLoginData({
+        ...loginData,
+        isError: err.response.data.errors[0],
+        isLoggedIn: false,
+      });
+    }
+  }
+
   return (
-    <AuthContext.Provider value={{ logInUser, loginData }}>
+    <AuthContext.Provider value={{ logInUser, signUpUser, loginData }}>
       {children}
     </AuthContext.Provider>
   );
--- src/Pages/SignUpPage.jsx
+++ src/Pages/SignUpPage.jsx
@@ -1,29 +1,94 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import "./SignUpPage.css";
 import signUpImg from "../assets/SignUpPageImg.jpg";
 import logo from "../assets/Colorlogo.png";
+import { AuthContext } from "../Contexts/AuthContext";
 
 function SignUpPage() {
+  const { signUpUser, loginData } = useContext(AuthContext);
+  const [signUpFormData, setSignUpFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+  const [formInputError, setFormInputError] = useState();
+  const navigate = useNavigate();
+
+  function handleSignUpFormSubmit(e) {
+    e.preventDefault();
+    if (
+      !signUpFormData.firstName.trim() ||
+      !signUpFormData.lastName.trim() ||
+      !signUpFormData.email.trim() ||
+      !signUpFormData.password.trim() ||
+      !signUpFormData.confirmPassword.trim()
+    ) {
+      setFormInputError(
+        "Error: Incomplete Form Submission. Please fill in all required fields."
+      );
+      return;
+    } else if (signUpFormData.password !== signUpFormData.confirmPassword) {
+      setFormInputError(
+        "Error: Passwords do not match. Please ensure that the password and confirm password fields are the same."
+      );
+      return;
+    }
+    signUpUser(
+      signUpFormData.email.trim(),
+      signUpFormData.password.trim(),
+      signUpFormData.firstName.trim(),
+      signUpFormData.lastName.trim()
+    );
+  }
+
+  const formInputErrorDisplay = formInputError && (
+    <p className="form-input-error"> {formInputError} </p>
+  );
+
+  const apiErrorDisplay = loginData.isError && (
+    <p className="form-input-error"> {loginData.isError} </p>
+  );
+
+  useEffect(() => {
+    if (loginData.isLoggedIn) {
+      navigate("/");
+    }
+  }, [loginData.isLoggedIn, navigate]);
+
   return (
     <div className="signup-page">
       <div className="content-box">
         <div className="branding-logo">
           <img src={logo} alt="the-bookshelf-logo" />
         </div>
-        <div className="form-fields">
+        <form className="form-fields" onSubmit={handleSignUpFormSubmit}>
           <h1 className="signup-page-heading">Sign Up</h1>
           <div className="input-name-fields">
             <input
               type="text"
               className="input-name-field"
               placeholder="First Name"
+              onChange={(e) =>
+                setSignUpFormData((prev) => ({
+                  ...prev,
+                  firstName: e.target.value,
+                }))
+              }
               required
             />
             <input
               type="text"
               className="input-name-field"
               placeholder="Last Name"
+              onChange={(e) =>
+                setSignUpFormData((prev) => ({
+                  ...prev,
+                  lastName: e.target.value,
+                }))
+              }
               required
             />
           </div>
@@ -32,22 +97,44 @@ function SignUpPage() {
             type="email"
             className="input-email-field"
             placeholder="Email"
+            onChange={(e) =>
+              setSignUpFormData((prev) => ({
+                ...prev,
+                email: e.target.value,
+              }))
+            }
             required
           />
           <input
             type="password"
             className="input-password-field"
             placeholder="Create Password"
+            onChange={(e) =>
+              setSignUpFormData((prev) => ({
+                ...prev,
+                password: e.target.value,
+              }))
+            }
             required
           />
           <input
             type="password"
             className="input-password-field"
-            placeholder="Re-enter Password"
+            placeholder="Confirm Password"
+            onChange={(e) =>
+              setSignUpFormData((prev) => ({
+                ...prev,
+                confirmPassword: e.target.value,
+              }))
+            }
             required
           />
-          <button className="signup-button">Sign Up</button>
-        </div>
+          <button className="signup-button" type="submit">
+            Sign Up
+          </button>
+        </form>
+        {formInputErrorDisplay}
+        {apiErrorDisplay}
         <div className="existing-user-prompt">
           Existing user? <Link to="/login">Log In</Link>
         </div>
