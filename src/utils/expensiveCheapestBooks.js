76c5c1117aed017b6b3d86878a14752d14f04c08
Manan Khurana <112554344+khuranamanan@users.noreply.github.com>
2023-05-21 12:36:06 +0530

    Delete expensiveCheapestBooks.js

--- src/utils/expensiveCheapestBooks.js
+++
@@ -1,8 +0,0 @@
-function expensiveBookInCollection(data) {
-  return data.reduce(
-    (result, book) => (result < book.price ? book.price : result),
-    0
-  );
-}
-
-export { expensiveBookInCollection };
