6d05c4ea639ad43f33d5df705a2466a5575831ef
Manan Khurana <112554344+khuranamanan@users.noreply.github.com>
2023-06-01 13:14:17 +0530

    feat: Order Address Selection on Checkout

--- src/Pages/Checkout/CheckoutPage.jsx
+++ src/Pages/Checkout/CheckoutPage.jsx
@@ -4,11 +4,51 @@ import { useNavigate } from "react-router";
 import useDocumentTitle from "../../Hooks/useDocumentTitle";
 import "./CheckoutPage.css";
 import CheckoutSummary from "./CheckoutSummary";
+import { AuthContext } from "../../Contexts/AuthContext";
+import { OrderContext } from "../../Contexts/OrderConext";
+import { ACTION_TYPES } from "../../utils/constant";
 
 function CheckoutPage() {
+  const { loginData } = useContext(AuthContext);
   const { cart } = useContext(BooksDataContext);
+  const { orderAddress, orderDispatch } = useContext(OrderContext);
   const navigate = useNavigate();
   useDocumentTitle("Checkout | The Bookshelf");
+
+  function handleAddressSelection(addressSelected) {
+    orderDispatch({
+      type: ACTION_TYPES.SET_ORDER_ADDRESS,
+      payload: { ...addressSelected },
+    });
+  }
+
+  const addressOptionMapped =
+    loginData?.user?.userAddresses.length !== 0 ? (
+      loginData?.user?.userAddresses.map((address) => (
+        <div key={address.id} className="address-option">
+          <label>
+            <input
+              type="radio"
+              name="address"
+              value={address.id}
+              checked={orderAddress.id === address.id}
+              onChange={() => handleAddressSelection(address)}
+            />
+            <span>
+              {address.firstName} {address.lastName}
+              <br />
+              {address.addressLine1}, {address.addressLine2}
+              <br />
+              {address.city}, {address.state}, {address.zipCode}
+              <br />
+              {address.country}
+            </span>
+          </label>
+        </div>
+      ))
+    ) : (
+      <p>No Addresses Added</p>
+    );
 
   const checkoutSummary = cart.length !== 0 && (
     <>
@@ -28,7 +68,16 @@ function CheckoutPage() {
         <h1>Checkout</h1>
       </div>
       <div className="checkout-page-content">
-        <div className="checkout-page-items">{/* {cartMapped} */}</div>
+        <div className="checkout-page-items">
+          <h2>Select Address</h2>
+          <div className="address-options">{addressOptionMapped}</div>
+          <button
+            className="add-address-btn btn-secondary"
+            onClick={() => navigate("/profile/addresses")}
+          >
+            Add/Edit Address
+          </button>
+        </div>
         {checkoutSummary}
       </div>
     </div>
